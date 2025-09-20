46452ea27244ae8916c5bd168aafb3f04d6a211b
João Victor Segato Bonifacio <81814410+jonny-bot@users.noreply.github.com>
2025-09-20 11:58:54 -0300

    Add files via upload

---
+++ Exercicios/20.09.2025/Tabuada_For.py
@@ -0,0 +1,4 @@
+tabuada = int(input('Digite uma Tabuada: '))
+
+for i in range(1, 10 + 1):
+    print(f'{tabuada} x {i} = {tabuada * i}')
---
+++ Exercicios/20.09.2025/Tabuada_While.py
@@ -0,0 +1,7 @@
+tabuada = int(input('Digite uma Tábuada: '))
+
+i = 1
+
+while i < 10 + 1:
+    print(f'{tabuada} x {i} = {tabuada * i}')
+    i += 1
