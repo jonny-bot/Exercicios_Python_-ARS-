2b3a388b15236c9ffcf93c9b8e7106f9eb20bf30
João Victor Segato Bonifacio <81814410+jonny-bot@users.noreply.github.com>
2025-11-24 09:17:10 -0300

    Add files via upload

---
+++ Exercicios/22.11.2025/main.py
@@ -0,0 +1,46 @@
+import random
+
+
+def gerar_numero():
+    return random.randint(1,100)
+
+
+escolha_maquina = gerar_numero()
+
+
+contador = 0
+
+total_jogadas = int(input('Digite o Total de Jogas: '))
+
+while contador < total_jogadas:
+
+    while True:
+        try:
+
+            escolha_jogador = int(input(f'Jogada {contador + 1} de {total_jogadas}\n'
+                                        'Tente Adivinhar o Numero que Máquina Escolheu: '))
+
+            if 1 <= escolha_jogador <= 100:
+
+                if escolha_jogador == escolha_maquina:
+                    print('Parabêns. Você Acertou!!!')
+                    break
+
+                elif escolha_jogador > escolha_maquina:
+                    print('Você Chutou um Número Maior!!!')
+
+                else:
+                    print('Você Chutou um Número Menor!!!')
+
+                contador += 1
+
+                if contador == total_jogadas:
+                    print('Que Pena. Acabou as suas Chances!!!\n'
+                          f'O Numero que a Máquina escolheu é o: {escolha_maquina}')
+                    break
+
+            else:
+                print(f'Você Digitou {escolha_jogador}. Que não está entre 1 e 100.')
+
+        except ValueError:
+            print('Digite uma Entrada Válida!!!')
