c8660e7ebc0e1a5297fcc9a6bcc8759bd49343b8
João Victor Segato Bonifacio <81814410+jonny-bot@users.noreply.github.com>
2026-04-09 16:45:38 -0300

    Add files via upload

---
+++ Exercicios/09.04.2026/teste.py
@@ -0,0 +1,8 @@
+lista = {0:'Saindo do Programa',
+         1:'Realizar Deposito',
+         2:'Realizar Saque',
+         3:'Realizar Tranferencia',
+         4:'Consultar Saldo Atual'}
+
+for i in lista:
+    print(f'{i} - {lista[i]}')
